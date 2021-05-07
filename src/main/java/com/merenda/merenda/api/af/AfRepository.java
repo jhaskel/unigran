5bbf1f05d35b214b6f46956221e7daff0b157b44
Joao Haskel <johaskel@gmail.com>
2021-05-06 22:20:58 -0300

    alterações itens

--- src/main/java/com/merenda/merenda/api/af/AfRepository.java
+++ src/main/java/com/merenda/merenda/api/af/AfRepository.java
@@ -7,14 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface AfRepository extends JpaRepository<Af, Long> {
-    @Query(value = "SELECT * FROM af WHERE ativo = TRUE ORDER BY id desc", nativeQuery = true)
+    @Query(value = "SELECT * FROM af WHERE isativo = TRUE ORDER BY id desc", nativeQuery = true)
     List<Af> findAll();
 
     @Query(value = "SELECT * FROM af\n" +
-            " WHERE fornecedor = :fornecedor AND ativo = TRUE AND isautorizado = true order by id desc", nativeQuery = true)
+            " WHERE fornecedor = :fornecedor AND isativo = TRUE AND isautorizado = true order by id desc", nativeQuery = true)
     List<Af> findByFornecedor(Long fornecedor);
 
-    @Query(value = "SELECT COUNT(id) as total FROM af WHERE ativo = TRUE AND isautorizado = false", nativeQuery = true)
+    @Query(value = "SELECT COUNT(id) as total FROM af WHERE isativo = TRUE AND isautorizado = false", nativeQuery = true)
     long findAf();
 
 
