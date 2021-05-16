29b0f7b20024f8a534bbdf980a55a1c2c6fb0a91
Joao Haskel <johaskel@gmail.com>
2021-05-16 15:44:38 -0300

    compras af

--- src/main/java/com/merenda/merenda/api/carros/CarroService.java
+++ src/main/java/com/merenda/merenda/api/carros/CarroService.java
@@ -28,8 +28,6 @@ public class CarroService {
         return rep.findByTipo(tipo).stream().map(CarroDTO::create).collect(Collectors.toList());
     }
 
-
-
     public CarroDTO insert(Carro carro) {
         Assert.isNull(carro.getId(),"Não foi possível inserir o registro");
         return CarroDTO.create(rep.save(carro));
@@ -56,30 +54,6 @@ public class CarroService {
             //throw new RuntimeException("Não foi possível atualizar o registro");
         }
     }
-
-    public CarroDTO update2(Carro carro, Long id) {
-        Assert.notNull(id,"Não foi possível atualizar o registro");
-
-        // Busca o carro no banco de dados
-        Optional<Carro> optional = rep.findById(id);
-        if(optional.isPresent()) {
-            Carro db = optional.get();
-            // Copiar as propriedades
-            db.setNome(carro.getNome());
-            db.setTipo(carro.getTipo());
-            System.out.println("Carro id " + db.getId());
-
-            // Atualiza o carro
-            rep.save(db);
-
-            return CarroDTO.create(db);
-        } else {
-            return null;
-            //throw new RuntimeException("Não foi possível atualizar o registro");
-        }
-    }
-
-
 
     public void delete(Long id) {
         rep.deleteById(id);
