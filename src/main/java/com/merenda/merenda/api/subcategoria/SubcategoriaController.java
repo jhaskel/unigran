1397e100013d2074b1e78537ea99de877e01de8a
Jhaskel <johaskel@gmail.com>
2021-10-12 07:43:22 -0300

    subcategoria

--- src/main/java/com/merenda/merenda/api/subcategoria/SubcategoriaController.java
+++ src/main/java/com/merenda/merenda/api/subcategoria/SubcategoriaController.java
@@ -9,7 +9,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/categorias")
+@RequestMapping("/api/v1/subcategorias")
 public class SubcategoriaController {
     @Autowired
     private SubcategoriaService service;
